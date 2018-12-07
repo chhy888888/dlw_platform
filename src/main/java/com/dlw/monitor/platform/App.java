e585b47774671caf1e0acc3ae74000cac6672b88
chhy <chhy@163.com>
2018-12-07 12:29:37 +0800

    dfd

---
+++ src/main/java/com/dlw/monitor/platform/App.java
@@ -0,0 +1,13 @@
+package com.dlw.monitor.platform;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
